79a9bd04773c371ea440d428d709fa4fa6d4b4e6
Debora Nozza <nozza.debora@gmail.com>
2020-09-07 21:46:44 +0200

    Set up simple shiny app

--- inst/shiny-examples/model_app/app.R
+++ inst/shiny-examples/model_app/app.R
@@ -1,74 +1,103 @@
 library(shiny)
-library(shinycssloaders)
-library(testpackage)
-library(dplyr)
-library(ggplot2)
+library("tools")
+library(DT)
 
-TARGET_VARIABLE <- "median_house_value"
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-        tabsetPanel(
-            tabPanel("Data", tableOutput("data_table")),
-            tabPanel("Model",
-                sidebarLayout(
-                    sidebarPanel(
-                        selectInput("model_type",
-                                    "Type of model:",
-                                    choices = "loading...",
-                                    selected = "loading..."),
-                        checkboxGroupInput("features", "Features included:",
-                                           choices = "loading...",
-                                           selected = NA)
-                    ),
+server <- shinyServer(function(input, output, session) {
 
-                    # Show a plot of the generated distribution
-                    mainPanel(
-                        plotOutput("scatter_plot") %>%
-                            withSpinner(color="#ee3224")
-                    )
-                )
-            )
+  userFile <- reactive({
+    validate(need(input$filedata !="", "Please import a data file"))
+    input$filedata
+  })    
+  
+  data <- reactive({
+    if(is.null(userFile()$datapath)) return(NULL)
+    utils::read.table(userFile()$datapath,
+                      header = input$header,
+                      sep = input$sep,
+                      row.names = NULL,
+                      #skip = 1,
+                      stringsAsFactors = FALSE)
+    
+  })
+  
+  output$table <- renderDT(
+    data()
+  )
+  
+  output$fileUploaded <- reactive({
+    return(!is.null(data()))
+  })
+  outputOptions(output, 'fileUploaded', suspendWhenHidden=FALSE)
+  
+  output$annotators <- renderUI(selectInput('annotators', 'Select Annotators column', names(data()), selected = names(data())[2]))
+  output$items <- renderUI(selectInput('annotations', 'Select Items column', names(data()), selected = names(data())[3]))
+  output$annotations <- renderUI(selectInput('items', 'Select Annotations column', names(data()), selected = names(data())[4]))
+  
+  output$downloadData <- downloadHandler(
+    filename = function () {
+      paste("output",file_ext(userFile()$datapath), sep = ".")
+    },
+    content = function(file) {
+      write.table(data(), file, sep = input$sep, row.names = FALSE)
+    }
+  )
+})
+
+
+ui <- shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("MACE"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      fileInput(inputId = "filedata",
+                label = "Upload data. Choose csv, tsv or txt file",
+                multiple = FALSE,
+                accept = c("text/csv", 
+                           "text/comma-separated-values,text/plain",
+                           ".csv",
+                           ".tsv")),
+      wellPanel(
+        h4(helpText("Select the input parameters below")),
+        checkboxInput("header", "Header", value = TRUE),
+        radioButtons("sep", "Separator",
+                     choices = c(Comma=',',Semicolon=';',Tab='\t', Space=''),
+                     selected = "\t",
+                     inline=TRUE),
+        conditionalPanel(
+          'output.fileUploaded',
+          #h4("Select Annotation column"),
+          uiOutput("annotators"),
+          uiOutput("items"),
+          uiOutput("annotations")
         )
+      ),
+      conditionalPanel(
+        'output.fileUploaded',
+        wellPanel(
+          h4(helpText("Select the MACE parameters below")),
+          sliderInput("iter", "Iterations:",
+                      min = 0, max = 5000,
+                      value = 2000, step = 500),
+          
+        ),
+        
+        downloadButton("downloadData", "Download")
+        #actionButton("runButton", "Run Mace", width = '100%',class = "btn-primary"),
+      ),
+    ),
+    
+    mainPanel(
+      tabsetPanel(type = "tabs",
+                  tabPanel("Input",DTOutput(outputId = "table"))
+                  #tabPanel("Confidence Annotators",DTOutput(outputId = "table_confidence"))
+      )
+      
+      
     )
+  )
+))
 
-# Define server logic required to draw a histogram
-server <- function(input, output, session) {
-
-    initialise_model <-
-        reactive(Model$new("data/housing.csv", TARGET_VARIABLE))
-
-    # update model choices
-    observe({
-        model_choices <- initialise_model()$algos
-
-        updateSelectInput(session, "model_type", choices = model_choices,
-                          selected = model_choices[1])
-
-        features <- names(initialise_model()$data)
-        features <- features[features != TARGET_VARIABLE]
-
-        updateCheckboxGroupInput(session, "features", choices = features,
-                                 selected = features)
-
-        })
-
-    output$data_table <- renderTable(initialise_model()$data)
-
-    output$scatter_plot <- renderPlot({
-        model <- initialise_model()
-
-        validate(need(input$model_type %in% model$algos,
-                 "Algorithm not in algorithm list"))
-        model_output <- model$fit_model(input$model_type, input$features)
-
-        model_output$predictions %>%
-            ggplot(aes(actual, preds)) +
-            geom_point() +
-            ggtitle(paste0("Predicted vs Actual, R-Squared: ",
-                           round(model_output$r_squared, 2)))
-    })
-}
-
-# Run the application
 shinyApp(ui = ui, server = server)
